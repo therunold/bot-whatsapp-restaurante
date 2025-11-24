026cd3c317439cd4a62594a5ade6c80775a82d42
therunold <storekaite1@gmail.com>
2025-11-24 18:57:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -497,4 +497,4 @@ if __name__ == "__main__":
     print("Status: Iniciando servidor...")
     print("=" * 50)
 
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=10000)
